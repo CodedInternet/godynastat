62095d9529d018eb0920badfe825e74f56786a5d
Tom Price <tom@codedinternet.com>
2017-02-27 16:53:25 +0000

    Add prototype I2C sensor control

--- onboard/onboard/device.go
+++ onboard/onboard/device.go
@@ -6,6 +6,17 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"os"
+	"syscall"
+)
+
+const (
+	FRAMERATE     = 20
+	i2c_SLAVE     = 0x0703
+	SB_REG_VALUES = 0x0100
+	SB_ROWS       = 16
+	SB_COLS       = 24
+	SB_BITS	      = 16
 )
 
 type UARTMCU struct {
@@ -13,11 +24,24 @@ type UARTMCU struct {
 	lock sync.Mutex
 }
 
+type I2CBus struct {
+	fd   *os.File
+	lock sync.Mutex
+}
+
+type SensorBoard struct {
+	i2cBus      I2CBus
+	address     int
+	buf         []byte
+	zeroValue   uint16
+	scaleFactor float64
+}
+
 func (mcu *UARTMCU) Open(ttyName string) {
 	port, err := serial.Open(&serial.Config{
 		Address:  ttyName,
 		BaudRate: 115200,
-		Timeout: 0.5*time.Second,
+		Timeout:  0.5 * time.Second,
 	})
 
 	if err != nil {
@@ -51,4 +75,70 @@ func (mcu *UARTMCU) Get(i2cAddr int, cmd uint8) (value int32) {
 	// Process response for return
 	fmt.Sscanf(string(input), "%d", value)
 	return value
+}
+
+// I2C related functions
+func OpenI2C(dev string) *I2CBus {
+	fd, err := os.Open(dev)
+	if err != nil {
+		panic(err)
+	}
+
+	return &I2CBus{fd: fd}
+}
+
+func ioctl(fd, cmd, arg uintptr) (err error) {
+	_, _, e1 := syscall.Syscall6(syscall.SYS_IOCTL, fd, cmd, arg, 0, 0, 0)
+	if e1 != 0 {
+		err = e1
+	}
+	return
+}
+
+func (bus *I2CBus) Connect(i2cAddr int) {
+	if err := ioctl(bus.fd.Fd(), i2c_SLAVE, uintptr(i2cAddr)); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func (bus *I2CBus) Get(i2cAddr int, reg uint16, buf []byte) {
+	// perform bitbashing to get write command first
+	var wbuf []byte
+	wbuf[0] = byte(reg >> 8 & 0xff)
+	wbuf[1] = byte(reg & 0xff)
+
+	bus.lock.Lock()
+	bus.Connect(i2cAddr)
+	// Do write/read inside critical section
+	bus.fd.Write(wbuf)
+	bus.fd.Read(buf)
+
+	bus.lock.Unlock()
+}
+
+// Sensor Boards
+func (sb *SensorBoard) Update() {
+	for {
+		sb.i2cBus.Get(sb.address, SB_REG_VALUES, sb.buf)
+		time.Sleep(time.Second / FRAMERATE)
+	}
+}
+
+func (sb *SensorBoard) SetScale(zero, half, full uint16) {
+	var max, m1, m2 float64
+
+	// Calculate the proportions of the scaling
+	max = (2 ^ SB_BITS) - 1
+	m1 = float64(half) / (max / 2)
+	m2 = float64(full) / max
+
+	// Assign struct values
+	sb.zeroValue = zero
+	sb.scaleFactor = (m1 + m2) / 2
+}
+
+func (sb *SensorBoard) GetValue(row, col int) uint16 {
+	i := row*SB_COLS + col
+	return uint16(sb.buf[i])<<8 + uint16(sb.buf[i+1])
 }
