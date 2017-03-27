78d3b6cb466983ab887e89f3507e57f5204b0dbb
Tom Price <tom@codedinternet.com>
2017-03-27 01:04:47 +0100

    Update to releast58-arm branch until https://github.com/keroserene/go-webrtc/pull/61 is resolved

--- onboard/build.sh
+++ onboard/build.sh
@@ -7,8 +7,9 @@ echo "Installing dependencies"
 go get .
 
 # we need to patch webrtc for arm builds
-curl -o /go/src/github.com/keroserene/go-webrtc/webrtc-linux-arm.pc https://raw.githubusercontent.com/CodedInternet/go-webrtc/arm/webrtc-linux-arm.pc
-curl -o /go/src/github.com/keroserene/go-webrtc/lib/libwebrtc-linux-arm-magic.a https://raw.githubusercontent.com/CodedInternet/go-webrtc/arm/lib/libwebrtc-linux-arm-magic.a
+curl -o /go/src/github.com/keroserene/go-webrtc/peerconnection.go https://raw.githubusercontent.com/CodedInternet/go-webrtc/release58-arm/peerconnection.go
+curl -o /go/src/github.com/keroserene/go-webrtc/webrtc-linux-arm.pc https://raw.githubusercontent.com/CodedInternet/go-webrtc/release58-arm/webrtc-linux-arm.pc
+curl -o /go/src/github.com/keroserene/go-webrtc/lib/libwebrtc-linux-arm-magic.a https://raw.githubusercontent.com/CodedInternet/go-webrtc/release58-arm/lib/libwebrtc-linux-arm-magic.a
 
 echo "Building"
 go build
