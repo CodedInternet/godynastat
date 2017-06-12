40fd455fa7d00ed478c3f42df782132b021273bd
Tom Price <tom@codedinternet.com>
2017-06-12 20:25:15 +0100

    Start work on converting from a signaling server to direct access

---
+++ auth.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"github.com/asdine/storm"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pressly/chi/render"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var (
+	JWT_HMAC_SECRET []byte        = []byte("xWumOlRfhu+LBi2F2e1yF4FiaopQ5mr8klL4fpILnlI=")
+	JWT_LIFESPAN    time.Duration = time.Hour
+)
+
+//---
+// Structs
+//
+
+// Represents a local user
+type User struct {
+	ID       int    `storm:"increment"` // pk
+	Email    string `storm:"unique"`
+	Name     string
+	Password string
+	Admin    bool
+}
+
+// Sets the User.Password to the hashed value for the provided plain text
+func (u *User) SetPassword(pass []byte) {
+	hash, err := bcrypt.GenerateFromPassword(pass, bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	u.Password = string(hash)
+}
+
+// Compares User.Password with the provided plain text.
+// Returns values directly as provided by the bcrypt library for downstream processing.
+func (u *User) VerifyPassword(pass []byte) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), pass)
+}
+
+//---
+// Generic payloads
+//---
+
+// Login payload
+type LoginPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (l *LoginPayload) Bind(r *http.Request) error {
+	return nil
+}
+
+type JWTPayload struct {
+	SignedToken string `json:"token"`
+}
+
+//---
+// Helper functions
+//
+
+// Produce a standard format JWT token
+func newJWT(sub string) (ts string, err error) {
+	// Create the Claims
+	now := time.Now().UTC()
+	claims := jwt.StandardClaims{
+		Issuer:    ENV.JWT_ISSUER,
+		IssuedAt:  now.Unix(),
+		ExpiresAt: now.Add(JWT_LIFESPAN).Unix(),
+		Subject:   sub,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	return token.SignedString(JWT_HMAC_SECRET)
+}
+
+//---
+// Views
+//---
+
+// Login looks up a user, verifies password and returns response
+func Login(w http.ResponseWriter, r *http.Request) {
+	data := &LoginPayload{}
+	if err := render.Bind(r, data); err != nil {
+		render.Render(w, r, ErrInvalidRequest(err))
+		return
+	}
+
+	var user User
+	if err := ENV.DB.One("Email", data.Email, &user); err != nil {
+		if err == storm.ErrNotFound {
+			render.Render(w, r, ErrNotFound)
+			return
+		}
+		panic(err)
+	}
+
+	err := user.VerifyPassword([]byte(data.Password))
+	if err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			render.Render(w, r, ErrPermissionDenied(errors.New("Invalid password")))
+			return
+		}
+		render.Render(w, r, ErrRender(err))
+		return
+	}
+
+	tokenString, err := newJWT(user.Email)
+	if err != nil {
+		render.Render(w, r, ErrRender(err))
+		return
+	}
+
+	render.JSON(w, r, JWTPayload{tokenString})
+}
+
+// Provides a new token to the client
+func JWTRefresh(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	token := ctx.Value("jwt").(*jwt.Token)
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	tokenString, err := newJWT(claims.Subject)
+	if err != nil {
+		render.Render(w, r, ErrRender(err))
+		return
+	}
+
+	render.JSON(w, r, JWTPayload{tokenString})
+}
+
+//---
+// Authentication middleware
+//---
+
+var (
+	JWTEmpty = errors.New("Bearer token not provided")
+)
+
+func ValidateJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		var tokenStr string
+		var err error
+
+		// Get token from query params
+		tokenStr = r.URL.Query().Get("jwt")
+
+		// Get token from authorization header
+		if tokenStr == "" {
+			bearer := r.Header.Get("Authorization")
+			if len(bearer) > 7 && strings.ToUpper(bearer[0:6]) == "BEARER" {
+				tokenStr = bearer[7:]
+			}
+		}
+
+		// Get token from cookie
+		if tokenStr == "" {
+			cookie, err := r.Cookie("jwt")
+			if err == nil {
+				tokenStr = cookie.Value
+			}
+		}
+
+		// Token is required, cya
+		if tokenStr == "" {
+			render.Render(w, r, ErrUnauthorized(JWTEmpty))
+			return
+		}
+
+		// parse and validate le token
+		token, err := jwt.ParseWithClaims(tokenStr,
+			&jwt.StandardClaims{},
+			func(*jwt.Token) (interface{}, error) { return JWT_HMAC_SECRET, nil })
+
+		if err != nil {
+			// well this has gone badly
+			// make sure we are actually working with the correct subclass
+			var jwterr *jwt.ValidationError
+			jwterr = err.(*jwt.ValidationError)
+
+			err = errors.New("Invalid token")
+			switch jwterr.Errors {
+			case jwt.ValidationErrorExpired:
+				err = errors.New("Token has expired")
+				break
+			}
+
+			render.Render(w, r, ErrUnauthorized(err))
+			return
+		}
+
+		if token.Valid {
+			ctx = context.WithValue(ctx, "jwt", token)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			render.Render(w, r, ErrUnauthorized(err))
+		}
+	})
+}
--- main.go
+++ main.go
@@ -2,29 +2,146 @@ package main
 
 import (
 	"fmt"
+	"github.com/abiosoft/ishell"
+	"github.com/asdine/storm"
+	"github.com/caarlos0/env"
+	"github.com/pressly/chi"
+	"github.com/pressly/chi/middleware"
 	"log"
 	"net/http"
-	"./signaling"
-	"html/template"
-	"github.com/gorilla/mux"
+	"os"
+	"path/filepath"
 )
 
-var Table [10][16]int
+type EnvConfig struct {
+	JWT_ISSUER string `env:"RESIN_DEVICE_UUID" envDefault:"DEV"`
+	RESIN      bool   `env:"RESIN" envDefault:"0"`
+	DB         *storm.DB
+}
 
-func webrtcHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("html/webrtc.html"))
-	tmpl.Execute(w, Table)
+var (
+	ENV *EnvConfig
+)
+
+func init() {
+	// Load main config
+	ENV = new(EnvConfig)
+	env.Parse(ENV)
+
+	// setup database
+	// make sure to init all of the structs
+
+	// get db path, this depends on if we are running on a resin device
+	var dbFile string
+	if ENV.RESIN {
+		dbFile = "/data/live.db"
+	} else {
+		dbFile, _ = filepath.Abs("./tmp/dev.db")
+		dir := filepath.Dir(dbFile)
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			os.Mkdir(dir, 0755)
+		}
+	}
+
+	db, err := storm.Open(dbFile)
+	if err != nil {
+		panic(err)
+	}
+	ENV.DB = db
+
+	// call inits for each type
+	if err := ENV.DB.Init(&User{}); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	port := "0.0.0.0:8000"
 
-	r := mux.NewRouter()
-	r.StrictSlash(true)
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	//r := mux.NewRouter()
+	//r.StrictSlash(true)
+	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	//
+	//r.PathPrefix("/ws/").Handler(signaling.Handler())
 
-	r.PathPrefix("/ws/").Handler(signaling.Handler())
-	r.HandleFunc("/app/", webrtcHandler)
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.RedirectSlashes)
+	r.Use(middleware.Recoverer) // make sure this is last
+
+	defer ENV.DB.Close() // close database when finished
+
+	//---
+	// Create a local shell
+	//---
+	{
+		shell := ishell.New()
+		shell.Println("Dynastat development shell")
+		shell.ShowPrompt(true)
+		shell.AddCmd(&ishell.Cmd{
+			Name: "createsuperuser",
+			Help: "createsuperuser <email> <password>",
+			Func: func(c *ishell.Context) {
+				// disable the '>>>' for cleaner same line input.
+				c.ShowPrompt(false)
+				defer c.ShowPrompt(true) // yes, revert when done.
+
+				// get email
+				var email string
+				if len(c.Args) >= 1 {
+					email = c.Args[0]
+				} else {
+					c.Print("Email: ")
+					email = c.ReadLine()
+				}
+
+				// get password
+				var password string
+				if len(c.Args) >= 2 {
+					password = c.Args[1]
+				} else {
+					c.Print("Password: ")
+					password = c.ReadPassword()
+				}
+
+				// create user
+				user := &User{
+					Email: email,
+					Name:  email,
+					Admin: true,
+				}
+				user.SetPassword([]byte(password))
+				err := ENV.DB.Save(user)
+				if err != nil {
+					panic(err)
+				}
+
+				c.Println("Superuser created")
+			},
+		})
+		go shell.Start()
+	}
+
+	// Build the API routes
+	r.Route("/api", func(r chi.Router) {
+		// login
+		r.Post("/login", Login)
+
+		r.Route("/", func(r chi.Router) {
+			// Seek, verify and validate JWT tokens
+			r.Use(ValidateJWT)
+
+			r.Get("/foo", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("Success"))
+			})
+
+			r.Get("/refresh_token", JWTRefresh)
+		})
+	})
 
 	fmt.Println("Listening on port", port)
 	if err := http.ListenAndServe(port, r); err != nil {
